bdf1c3e0c3e2de298fc5ab7908c3d21356baa855
Graham Smith <hedgy134117@gmail.com>
2021-05-07 10:59:24 -0400

    sketch15.js final

--- sketches/sketch15.js
+++ sketches/sketch15.js
@@ -2,11 +2,11 @@ class Thing {
   constructor(cx, cy) {
     this.cx = cx;
     this.cy = cy;
-    this.r = random(50, 200);
+    this.r = 100;
     this.za = Math.floor(random(1, 8));
     this.zb = Math.floor(random(1, 8));
     this.off = random(0, 1000);
-    this.inc = 1 / random([150, 200, 256, 512]);
+    this.inc = 1 / random([256, 512]);
   }
   
   display() {
@@ -14,12 +14,12 @@ class Thing {
     let n2 = noise(this.off + 2);
     let n3 = noise(this.off + 4);
     noFill();
-    stroke(100 + n * 128, 100 + n2 * 128, 100 + n3 * 128, 10);
+    stroke(n * 256, n2 * 256, n3 * 256, 255);
     for (let i = 0; i < 100; i++) {
-      line(this.cx + cos(i) * this.r + n * this.r, 
-            this.cy + sin(i) * this.r + n2 * this.r,
-            this.cx + cos(i + this.za) * this.r + n * this.r, 
-            this.cy + sin(i + this.zb) * this.r + n2 * this.r
+      line(this.cx + cos(i) * r + n * r, 
+            this.cy + sin(i) * r + n2 * r,
+            this.cx + cos(i + this.za) * r + n * r, 
+            this.cy + sin(i + this.zb) * r + n2 * r
            );  
     }
     this.za += n / 100;
@@ -35,17 +35,19 @@ class Thing {
 let t = [];
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0, 0, 0);
-	let steps = 100;
+  let steps = 100;
   for (let i = 0; i < width; i += steps) {
     t.push(new Thing(
-      random(0, width), random(0, height)
+      random(0, width - 100), random(0, height - 100)
     ));
   }
 }
 
 let r = 100;
 function draw() {
+  blendMode(ADD);
+  background(255, 255, 255, 1);
+  blendMode(BLEND);
   for (let i = 0; i < t.length; i++) {
     t[i].display();
   }
