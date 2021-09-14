3bea29962ca9acb124447f4c6c16d46434c36eff
Graham Smith <hedgy134117@gmail.com>
2021-09-14 11:42:29 -0400

    the_void_of_squares.js

---
+++ sketches/code/the_void_of_squares.js
@@ -0,0 +1,19 @@
+
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+let i = 0;
+let j = 270;
+function draw() {
+  translate(width / 2, height / 2);
+  scale(sin(j) + 1);
+  for (let x = 0; x <= width; x += 1) {
+    rotate(sin(x + i * 2) * 360);
+    square(x, noise(x / 50 + i) * 500, 20);    
+  }
+  i += 0.003;
+  j++;
+}
